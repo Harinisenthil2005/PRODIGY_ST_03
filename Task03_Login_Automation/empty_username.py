9a04f3f11088e5dccef9174762a3e2a64813aa29
Harinisenthil2005 <144198954+Harinisenthil2005@users.noreply.github.com>
2026-06-12 18:10:19 +0530

    Add files via upload

---
+++ Task03_Login_Automation/empty_username.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.saucedemo.com/")
+
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+time.sleep(2)
+
+driver.save_screenshot("empty_username.png")
+
+print("PASS")
+
+driver.quit()
